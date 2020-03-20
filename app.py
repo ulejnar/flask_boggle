6d9999ff2fefe72ce4fdef49787ee22bf1bf78a5
Terry Chiem <terrychiem@gmail.com>
2020-03-20 12:15:26 -0700

    complete step 1

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+from flask import Flask, request, render_template, redirect
+from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
 boggle_game = Boggle()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+
+debug = DebugToolbarExtension(app)
+
+@app.route('/')
+def show_home_page():
+    """ Show the home page """
+    
+    return "Hello"
