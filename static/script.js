14a6f34f7c5c9c04060c17dc54df3b68e6fa2a6f
ulejnar <ulejnar@yahoo.de>
2020-03-20 15:50:13 -0700

    end of step five

--- static/script.js
+++ static/script.js
@@ -3,14 +3,26 @@ $(function() {
   $submitButton = $('#submit');
   $guessInput = $('#guess');
   $result = $('#result');
+  $gameOverMsg = $('#gameOverMsg')
+  let score =0;
 
   $submitButton.on('click', async function(e){
     e.preventDefault();
 
     let guess = $guessInput.val();
     let response = await axios.get(`${BASE_URL}/make-guess?guess=${guess}`);
+    $guessInput.val("")
 
     // display result from JSON response
-    $result.text(response.data.result)
+    if(response.data.result =="ok"){
+      score+=guess.length
+    }
+    $result.text(score)
   });
+
+  setTimeout(function(){
+    $submitButton.prop("disabled", true)
+    $gameOverMsg.text("Time is out")
+    }, 6000)
+    
 });
